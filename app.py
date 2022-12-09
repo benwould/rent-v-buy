461f971d740c91919d663f0fbc32dd37e9ff13e4
Ben Wood <benwould@gmail.com>
2022-12-08 19:52:40 -0800

    push tax rate in app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ import matplotlib
 def get_main_df():
     #reading the csv, setting the linebreak as 'tab', setting datetime format for the Month index, and setting three indexes (month, region, proptype)
     main_df = pd.read_csv(Path('data/redfin_table.csv'), on_bad_lines='skip', sep='\t', infer_datetime_format=True, parse_dates=True, index_col=['Month', 'Region'])
-    
+
     main_df.sort_index(level=['Month', 'Region'], ascending=[1, 0], inplace=True)
     #drop unnecessary column
     main_df = main_df.drop(['For-Sale Property Type'], axis=1)
-    
+
     #removing the '$' '%' ',' from the datapoints so they can be converted to ints as needed
     main_df.loc[ : , 'Average Monthly Rent'] = main_df.loc[ : , 'Average Monthly Rent'].str.replace(r'$', '', regex=True)
     main_df.loc[ : , 'Monthly mortgage, 5% down'] = main_df.loc[ : , 'Monthly mortgage, 5% down'].str.replace(r'$', '', regex=True)
@@ -35,11 +35,9 @@ def get_main_df():
     main_df['Monthly mortgage, 5% down YoY']=main_df['Monthly mortgage, 5% down YoY'].astype('float')
     main_df['Monthly mortgage, 20% down YoY']=main_df['Monthly mortgage, 20% down YoY'].astype('float')
     main_df['Median sale price YoY']=main_df['Median sale price YoY'].astype('float')
-    
+
     #shorten the DF to only include chosen cities
     main_df = main_df.loc[slice(None), ['Austin, TX', 'San Diego, CA', 'Atlanta, GA', 'Washington, DC', 'West Palm Beach, FL', 'Indianapolis, IN', 'Pittsburg, PA', 'Detroit, MI', 'Denver, CO', 'Boston, MA'], : ]
-
-
     return main_df
 
 def get_city_list():
